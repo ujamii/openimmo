5ba3e1eabb8b9d99f13f4429d2432f33d7c7efca
Michael Grundkötter <mg@ujamii.com>
2021-02-20 22:47:42 +0100

    [TEST] as we now have 100% coverage, lets kill the last mutants

---
+++ tests/Generator/ApiGenerator/TypeWithInconsistentNamingTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Ujamii\OpenImmo\Tests\Generator\ApiGenerator;
+
+class TypeWithInconsistentNamingTest extends FileGeneratingTest
+{
+
+    public function testGenerateApiClassWithDateTypes(): void
+    {
+        $generatedClass = $this->getGeneratedClassFromFile(
+            'type_with_inconsistent_naming'
+        );
+
+        $properties = [
+            self::getPropertyConfig('uppercaseftw', 'bool', true, ['SerializedName' => '("UPPERCASEFTW")']),
+        ];
+
+        $this->assertClassHasProperties($generatedClass, $properties);
+        $this->assertTrue($generatedClass->hasUseStatement('JMS\Serializer\Annotation\SerializedName'));
+    }
+
+}
