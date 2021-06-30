7ee29a95b23b4a865d8a97a48589b5bb4dddca21
Michael Grundkötter <mg@ujamii.com>
2021-06-30 08:59:13 +0200

    [BUGFIX] dont remove phpdoc tags with rector for backwards compat in the api generation process

--- build/config/rector.php
+++ build/config/rector.php
@@ -3,15 +3,19 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
-use Rector\Set\ValueObject\SetList;
 use Rector\DeadCode\Rector\ClassConst\RemoveUnusedPrivateClassConstantRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(SetList::DEAD_CODE);
 
-    // this is removed as all the DTO object constants are not used inside the library itself
-    // get parameters
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::SKIP, [RemoveUnusedPrivateClassConstantRector::class]);
+    $parameters->set(Option::SKIP, [
+        RemoveUnusedPrivateClassConstantRector::class,
+        RemoveUselessParamTagRector::class,
+        RemoveUselessReturnTagRector::class
+    ]);
 };
