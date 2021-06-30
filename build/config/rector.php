43f9f0ecfb1dd4b7bfec1c972e38cfa792c9f5f1
Michael Grundkötter <mg@ujamii.com>
2021-06-30 08:25:10 +0200

    [CI] fixed build pipeline

--- build/config/rector.php
+++ build/config/rector.php
@@ -4,18 +4,14 @@ declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
 use Rector\Set\ValueObject\SetList;
-use Rector\DeadCode\Rector\ClassConst\RemoveUnusedClassConstantRector;
+use Rector\DeadCode\Rector\ClassConst\RemoveUnusedPrivateClassConstantRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-
-    // Define what rule sets will be applied
-    $parameters->set(Option::SETS, [
-        SetList::DEAD_CODE,
-    ]);
+    $containerConfigurator->import(SetList::DEAD_CODE);
 
     // this is removed as all the DTO object constants are not used inside the library itself
-    $parameters->set(Option::SKIP, [RemoveUnusedClassConstantRector::class]);
+    // get parameters
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::SKIP, [RemoveUnusedPrivateClassConstantRector::class]);
 };
